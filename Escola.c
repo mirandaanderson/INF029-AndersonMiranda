17e445a40f69b2381d01fa1e04e1e3c1c1fc22ef
mirandaanderson <andersonmiranda180@gmail.com>
2025-04-24 02:46:24 +0000

    Inicio do primeiro menu

--- Escola.c
+++ Escola.c
@@ -1,8 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
-#include <String.h>
+#include <string.h>
 
 int main() {
-  printf("Olá mundo!");
+  int opcao;
+  printf("########### Projeto Escola ###########\n\n");
+
+  while(opcao != 0) {
+    printf("0 - Sair\n");
+    printf("1 - Menu Aluno\n");
+    printf("2 - Menu Disciplina\n");
+    printf("3 - Menu Professor\n");
+    scanf("%d", &opcao);
+
+    switch(opcao) {
+      /*case 0: {
+        printf("Saindo...\n"); 
+        break;
+      }*/
+    }
+  }  
+
 }
