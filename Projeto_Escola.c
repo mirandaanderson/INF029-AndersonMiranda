130afa6e273925c781e82f74399c309e3ae92379
Anderson Miranda <andersonmiranda180@gmail.com>
2025-04-30 21:27:37 -0300

    Declaração das variaveis do tipo struct.

--- Projeto_Escola.c
+++ Projeto_Escola.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Projeto_Escola.h>
+#include "Projeto_Escola.h"
 
-int main(){
-
-
+int main()
+{
+    // Declaração de variáveis
+    Aluno alunos[50];
+    Professor professores[50];
+    Disciplina disciplina[50];
+    int qtd_alunos = 0, qtd_professores = 0, qtd_disciplinas = 0;
+    int opcao = 0;
+   
 }
--- Projeto_Escola.h
+++ Projeto_Escola.h
@@ -12,12 +12,22 @@ typedef struct{
     char sexo;
     char data_nascimento[11]; // Formato: dd/mm/aaaa
     char cpf[15]; // Formato: 000.000.000-00
-} Profressor;
+} Professor;
 
 typedef struct {
     char nome[50];
     int codigo;
     int semestre;
     int professor_matricula; // Matricula do professor
-    int alunos[50]; 
+    int alunos_matricula[50]; // Matricula dos alunos
 } Disciplina;
+
+int menu_principal() {
+    int opcao;
+    printf("########Sistema de Gerenciamento Escolar########\n\n");
+    printf("1. Cadastrar Aluno\n2. Cadastrar Professor\n3. Cadastrar Disciplina\n4. Listar Alunos\n5. Listar Professores\n6. Listar Disciplinas\n0. Sair\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+    return opcao;
+}
+
