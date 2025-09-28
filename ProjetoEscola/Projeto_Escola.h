680e5fce550374553ef6d13b517b3de56d91f55a
mirandaanderson <andersonmiranda180@gmail.com>
2025-09-28 15:11:58 +0000

    Recomeçando o projeto escola. Iniciando a configuração do header.

--- ProjetoEscola/Projeto_Escola.h
+++ ProjetoEscola/Projeto_Escola.h
@@ -1,3 +1,6 @@
+#ifndef PROJETO_ESCOLA.H
+#define PROJETO_ESCOLA.H
+
 typedef struct
 {
     int matricula;
@@ -25,49 +28,11 @@ typedef struct
     int alunos_matricula[50]; // Matricula dos alunos
 } Disciplina;
 
-int menu_principal()
-{
-    int opcao;
-    printf("########Sistema de Gerenciamento Escolar########\n\n");
-    printf("1. Cadastrar Aluno\n2. Cadastrar Professor\n3. Cadastrar Disciplina\n4. Listar Alunos\n5. Listar Professores\n6. Listar Disciplinas\n0. Sair\n");
-    printf("Escolha uma opcao: ");
-    scanf("%d", &opcao);
-    return opcao;
-}
 
-int cadastrar_aluno(Aluno *alunos, int qtd_alunos)
-{
-    int valido = 0;
+void menu_principal(int *opcao);
 
-    printf("Insira às informações do aluno:\n\n");
-    printf("Nome: ");
-    fgets(alunos[qtd_alunos].nome, sizeof(alunos[qtd_alunos].nome), stdin);
-    alunos[qtd_alunos].nome[strcspn(alunos[qtd_alunos].nome, "\n")] = "\0"; // Troca o '\n' por '\0'
-    printf("Matricula: ");
-    scanf("%d", &alunos[qtd_alunos].matricula);
-    printf("Sexo (M/F): ");
-    scanf(" %c", &alunos[qtd_alunos].sexo);
-    printf("Dara de Nascimnto (DD/MM/AAAA): ");
-    scanf("%s", &alunos[qtd_alunos].data_nascimento);
-    printf("CPF (000.000.000-00):");
-    scanf("%s", &alunos[qtd_alunos].cpf);
-    while (valido != "1")
-    {
-    }
-}
+void menu_secundario (char set_menu);
 
-int validar_cpf(char *cpf)
-{
-    int i = 0;
-    int soma_digitos = 0;
-    int digito1 = 0;
-    int digito2 = 0;
-    int tam = 13;
-    while (i < tam)
-    {
-        if (cpf != "." || cpf != "-") {
-         
-            soma_digitos += (int) cpf[i] * 10 - i;
-        }
-    }
-}
+int validar_cpf(int);
+
+#endif //PROJETO_ESCOLA.H
