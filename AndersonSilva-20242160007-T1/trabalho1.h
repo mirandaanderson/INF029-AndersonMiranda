c3652643cac384eed7a8f8df06bfdf9678251c41
mirandaanderson <andersonmiranda180@gmail.com>
2025-11-19 19:23:51 -0300

    Atualizar o trabalho1.h

--- AndersonSilva-20242160007-T1/trabalho1.h
+++ AndersonSilva-20242160007-T1/trabalho1.h
@@ -22,33 +22,28 @@
 #ifndef TRABALHO1_H
 #define TRABALHO1_H
 
-typedef struct DQ
-{
-    int iDia; 
+typedef struct {
+    int iDia;
     int iMes;
     int iAno;
-    int valido; // 0 se inválido, e 1 se válido 
-
+    int valido;
 } DataQuebrada;
 
-typedef struct Qtd
-{
+typedef struct {
     int qtdDias;
     int qtdMeses;
     int qtdAnos;
     int retorno;
-
 } DiasMesesAnos;
 
-int somar(int x, int y); //função utilizada para testes
-int fatorial(int x); //função utilizada para testes
 int q1(char data[]);
 DiasMesesAnos q2(char datainicial[], char datafinal[]);
 int q3(char *texto, char c, int isCaseSensitive);
 int q4(char *strTexto, char *strBusca, int posicoes[30]);
 int q5(int num);
 int q6(int numerobase, int numerobusca);
-int q7(char matriz[8][10], char palavra[5]);
+int q7();
 DataQuebrada quebraData(char data[]);
+
 
 #endif  // MAIN_H
